f38201e5ad8eeda7031663d103aaf3d830c60cb4
heymanmillionaire <heymanmillionaire@proton.me>
2025-01-22 10:54:37 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,4 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git homeproxy https://github.com/heymanmillionaire/openwrt-homeproxy >> feeds.conf.default
