8dbf2a58b5ccff69f226d50df6bfed260baeafec
heymanmillionaire <heymanmillionaire@proton.me>
2025-01-22 15:05:11 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,7 +19,7 @@ echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.defa
 echo 'src-git homeproxy https://github.com/heymanmillionaire/openwrt-homeproxy >> feeds.conf.default
 
 # Install openclash
-svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
+# svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
 # mkdir openwrt/package/luci-app-openclash
 # cd openwrt/package/luci-app-openclash
 # git init
@@ -44,5 +44,8 @@ echo "src-git homeproxy https://github.com/muink/luci-app-homeproxy" >> feeds.co
 
 # Install mihomo
 echo "src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main" >> "feeds.conf.default"
+cat >> .config <<EOF
+CONFIG_DEFAULT_luci-app-mihomo
+EOF
 
         
